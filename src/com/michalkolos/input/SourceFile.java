fae2014e2640870f72bef6fabbb34ae8988a8c1c
michalkolos <cool.kolos@gmail.com>
2022-01-06 13:01:00 +0100

    SourceFile class that represents system files to be read and parsed.

---
+++ src/com/michalkolos/input/SourceFile.java
@@ -0,0 +1,55 @@
+package com.michalkolos.input;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Represents file asset that can be read in its entirety as a string or as a
+ * List of strings, where each string represents one line of the file.
+ */
+public class SourceFile {
+	private File file;
+
+
+	/**
+	 * @param path Absolute path to the file.
+	 */
+	public SourceFile(String path) {
+		file = new File(path);
+	}
+
+	/**
+	 * @param file File to be read.
+	 */
+	public SourceFile(File file) {
+		this.file = file;
+	}
+
+
+	/**
+	 * Reads whole content of the file as a String.
+	 * @return File contents as a String.
+	 * @throws IOException  Thrown when the file cannot be accessed.
+	 */
+	public String readString() throws IOException{
+
+		InputStream inputStream = new FileInputStream(this.file);
+		String fileContents = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+		inputStream.close();
+
+		return fileContents;
+	}
+
+
+	/**
+	 * Reads whole content of the file as a List of Strings
+	 * @return List of Strings representing individual lines of the file.
+	 * @throws IOException  Thrown when the file cannot be accessed.
+	 */
+	public List<String> readLines() throws IOException{
+		return Arrays.asList(readString().split("\n"));
+	}
+}

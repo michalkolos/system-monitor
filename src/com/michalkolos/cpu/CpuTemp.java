6992a7604064050a50a1239d29ed8ea3a20eb617
Michalkolos <cool.kolos@gmail.com>
2022-01-11 22:41:42 +0100

    Updated comments in CpuTemp.java.

--- src/com/michalkolos/cpu/CpuTemp.java
+++ src/com/michalkolos/cpu/CpuTemp.java
@@ -11,10 +11,18 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Gathers processor temperature data as reported by the system.
+ * Gathers CPU temperature data as reported by the system.
  */
 public class CpuTemp {
+
+	/**
+	 *  Possible names of subsystems providing temperature data.
+	 */
 	public static final String[] THERMAL_SUBSYSTEMS = {"k10temp", "coretemp", "cpu_thermal"};
+
+	/**
+	 *  Possible names of temperature files in the given subsystems.
+	 */
 	public static final String[] THERMAL_FIELDS = {"temp1_input"};
 
 	private final Hwmon hwmon = new Hwmon();
@@ -28,6 +36,11 @@ public class CpuTemp {
 	}
 
 
+	/**
+	 * Returns current CPU temp provided by system's Hwmon API.
+	 * @return Optional of temperature data. It will be empty if the system file
+	 * providing the data is inaccessible.
+	 */
 	public Optional<Float> checkTemp() {
 
 		return Utils.extractStringFromFileOptional(tempField)
@@ -35,6 +48,16 @@ public class CpuTemp {
 				.map(f->f/1000);
 	}
 
+
+	/**
+	 * Searches the Hwmon API for a file containing CPU temperature data by matching
+	 * subsystem name and field filename with given keywords.
+	 * @param subsystemNames List of viable subsystem names. Will be compared to
+	 *                       contents of "name" files of all available subsystems.
+	 * @param fieldNames List of all viable field filenames.
+	 * @return Returns Optional containing random File object from all matching
+	 * given criteria. Optional will be empty if there are no matching Files.
+	 */
 	private Optional<File> scanTempField(List<String> subsystemNames, List<String> fieldNames) {
 		return subsystemNames.stream()
 				.map(hwmon::getSubsystemOptional)

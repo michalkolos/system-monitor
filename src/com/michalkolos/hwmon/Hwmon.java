8add85319e8394d10bc5f530f78b5a83fe523a49
Michalkolos <cool.kolos@gmail.com>
2022-01-12 01:21:40 +0100

    Updated comments in Hwmon.java

--- src/com/michalkolos/hwmon/Hwmon.java
+++ src/com/michalkolos/hwmon/Hwmon.java
@@ -11,8 +11,15 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.util.*;
 
+
+/**
+ * Extracts hardware data from Hwmon system API.
+ */
 public class Hwmon {
 
+	/**
+	 * Absolute path to Hwmon directory.
+	 */
 	public static final String HWMON_PATH = "/sys/class/hwmon/";
 
 	public static final int FIELD_NAME_MAX_LEN = 40;
@@ -21,12 +28,21 @@ public class Hwmon {
 	private final Map<String, HwmonSubsystem> subsystems = new HashMap<>();
 
 
-	public Hwmon() {
+	public Hwmon() throws IOException {
 		scanSubsystems();
 	}
 
-	private void scanSubsystems() {
+
+	/**
+	 * Scans Hwmon file tree for subsystems and their fields.
+	 * @throws IOException Thrown when Hwmon root directory is inaccessible.
+	 */
+	private void scanSubsystems() throws IOException {
 		File hwmonDir = new File(HWMON_PATH);
+
+		if (!hwmonDir.exists()) {
+			throw new IOException("Unable to access Hwmon directory.");
+		}
 
 		FileFilter filter = file -> file.isDirectory() &&
 				file.getName().matches("hwmon[0-9]+");
@@ -45,66 +61,62 @@ public class Hwmon {
 			}
 		}
 
-//      TODO: delete bellow:
-//		for(File dir : subsystemDirs) {
-//			File nameFile = new File(dir.getAbsolutePath() + NAME_FILE);
-//
-//			Utils.extractStringFromFileOptional(nameFile).ifPresent(name ->
-//					subsystems.put(name, new HwmonSubsystem(dir, name)));
-//		}
 	}
 
-
-
-
-
+	/**
+	 * Returns names of all subsystems reported by the Hwmon API.
+	 * @return Set containing all subsystem names.
+	 */
 	public Set<String> getSubsystemNames() {
 		return subsystems.keySet();
 	}
 
+
+	/**
+	 * Returns names of fields in a given subsystem.
+	 * @param subsystemName Name of the subsystem.
+	 * @return  Returns an Optional containing field names in a Set. If the
+	 * subsystem name is invalid the Optional will be empty.
+	 */
 	public Optional<Set<String>> getFieldNamesOptional(String subsystemName) {
 		return Optional.ofNullable(subsystems.get(subsystemName))
 				.map(HwmonSubsystem::getFields)
 				.map(Map::keySet);
 	}
 
-//  TODO: Delete bellow:
-//	public String getFieldValue(String subsystemName, String fieldName) {
-//		HwmonSubsystem subsystem = subsystems.get(subsystemName);
-//		String fieldVal = null;
-//
-//		if(subsystem != null) {
-//			fieldVal = subsystem.getFields().get(fieldName).getValue();
-//		}
-//
-//		return fieldVal;
-//	}
-
+	/**
+	 * Returns value of a given field.
+	 * @param subsystemName Name of the subsystem.
+	 * @param fieldName Name of the field in given subsystem.
+	 * @return Optional containing either contents of the field's file or empty
+	 * if the field is inaccessible.
+	 */
 	public Optional<String> getFiledValueOptional (String subsystemName, String fieldName) {
 		return getFieldFileOptional(subsystemName, fieldName)
 				.flatMap(Utils::extractStringFromFileOptional);
 	}
 
+	/**
+	 * Returns file of a given field.
+	 * @param subsystemName Name of the subsystem.
+	 * @param fieldName Name of the field in given subsystem.
+	 * @return Optional containing either the field's file or empty
+	 * if the field is inaccessible.
+	 */
 	public Optional<File> getFieldFileOptional(String subsystemName, String fieldName) {
 		return getSubsystemOptional(subsystemName)
 				.map(subsystem -> subsystem.getFields().get(fieldName));
 	}
 
+	/**
+	 * Returns subsystem object corresponding to a given subsystem name.
+	 * @param subsystemName Name of the subsystem.
+	 * @return Optional containing either the HwmonSusystem object or empty
+	 * if the subsystem name is invalid.
+	 */
 	public Optional<HwmonSubsystem> getSubsystemOptional (String subsystemName) {
 		return Optional.ofNullable(subsystems.get(subsystemName));
 	}
-
-//	TODO: Delete bellow:
-//	public File getFieldFile(String subsystemName, String fieldName) {
-//		HwmonSubsystem subsystem = subsystems.get(subsystemName);
-//		File fieldFile = null;
-//
-//		if(subsystem != null) {
-//			fieldFile = subsystem.getFields().get(fieldName).getFile();
-//		}
-//
-//		return fieldFile;
-//	}
 
 
 	public String subsystemToString(String name) {
